6f773e4a702f5e5736558c5f32d2579e94c21ac2
dwizahira <dwizahira05@gmail.com>
2023-10-14 19:09:09 +0800

    first commit

---
+++ ifelse.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string hari ="Senin";
+    if(hari=="Senin"){
+        cout<<"Selamat hari senin"<<endl;
+    }
+    else if(hari=="Selasa"){
+        cout<<"Selamat hari selasa"<<endl;
+    }
+    else if(hari=="rabu"){
+        cout<<"Selamat hari rabu"<<endl;
+    }
+    else{
+        cout<<"Selamat hari minggu"<<endl;
+    }
+}
---
+++ pertemuan_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int telur = 3;
+    bool jagung = false;
+
+    if (jagung){
+        telur = 10;
+    }
+     else {
+        telur = 2;
+    }
+    cout<<"telur: "<<telur<<endl;
+}
